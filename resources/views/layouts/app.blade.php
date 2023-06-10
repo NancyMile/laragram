16bd82049b040831eec68b05eab9cd03c6773563
nancy <smithnancy@outlook.com>
2023-06-10 12:53:54 +1000

    installing and adding dropzone for uploads images on create

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Pagina - @yield('titulo')</title>
         @vite('resources/css/app.css')
+        <link rel="stylesheet" href="https://unpkg.com/dropzone@5/dist/min/dropzone.min.css" type="text/css" />
     </head>
     <body class="bg-gray-100">
         <header class="p-5 border-b bg-white shadow">
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,9 @@
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10">
-            imagen aqui
+            <form id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center items-center">
+
+            </form>
         </div>
         <div class="md:w-4/12 bg-white p-10 shadow-xl mt-10 md:mt-0">
             <form action="{{ route('register')}}" method="POST" novalidate>

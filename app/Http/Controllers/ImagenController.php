c80c47ab879335564cd8b5a93287f4fa1b8de7b2
nancy <smithnancy@outlook.com>
2023-06-10 19:16:08 +1000

    functionality  to save images on server

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
@@ -14,8 +16,21 @@ class ImagenController extends Controller
 
         $imagen = $request->file('file');
         // the following may return  something  like {"imagen":"jpg"}
-        return response()->json(['imagen' => $imagen->extension()]);
+        //return response()->json(['imagen' => $imagen->extension()]);
 
+
+        // con el uuid  vamos a  agregarle un aid a la  imagen  para hacerla unica
+        $nombreImagen = Str::uuid().".".$imagen->extension();
+        //crear la imagen el el servidor  import Image de los Facades
+        $imagenServidor = Image::make($imagen);
+        //vamos hacer  que cada imagen fit  1000X1000 pixels
+        $imagenServidor->fit(1000,1000);
+
+        $imagenPath = public_path('uploads').'/'.$nombreImagen;
+        //mueva la  ruta al servidor  una ve  precesada
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
 
      }
 }

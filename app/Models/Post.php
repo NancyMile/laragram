4d90058eb97ad7cbadc694feaaa1793edada6806
nancy <smithnancy@outlook.com>
2023-06-11 14:15:33 +1000

    stablising  relations  between user and post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -15,4 +17,9 @@ class Post extends Model
         'imagen',
         'user_id'
     ];
+
+    public function user(){
+        //un post pertenece a un usuario y  solo quiereo que devuelva el name and username
+        return $this->belongsTo(User::class)->select(['nome','username']);
+    }
 }

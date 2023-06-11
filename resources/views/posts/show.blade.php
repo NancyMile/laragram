084a32b74a997dbaf4f1aef3efd2284b17d583d3
nancy <smithnancy@outlook.com>
2023-06-11 22:27:59 +1000

    post show display image and description added

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,5 +5,26 @@
 @endsection
 
 @section('contenido')
-    {{ $post->descripcion }}
+    <div class="container mx-auto flex">
+        <div class="md:w-1/2">
+            <img src="{{ asset('uploads').'/'.$post->imagen }}" alt="Imagen post {{ $post->titulo }}"/>
+            <div class="p-3">
+                0 Likes
+            </div>
+            <div>
+                <p class="font-bold">
+                    {{ $post->user->username }}
+                </p>
+                <p class="text-sm text-gray-500">
+                    {{ $post->created_at->diffForHumans() }}
+                </p>
+                <p class="mt-5">
+                    {{ $post->descripcion }}
+                </p>
+            </div>
+        </div>
+        <div class="md:w-1/2">
+            2
+        </div>
+    </div>
 @endsection

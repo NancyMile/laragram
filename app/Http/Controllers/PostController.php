3984c68e77fa4b9d08921cceed03608366ff364e
nancy <smithnancy@outlook.com>
2023-06-12 20:48:31 +1000

    deleting the image from uloads  when the post is deleted

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -81,6 +82,14 @@ class PostController extends Controller
         //checkquea la poicy PostPolicy detele
         $this->authorize('delete',$post);
         $post->delete();
+
+        //borrar la imagen
+        $imagen_path = public_path('uploads/').$post->imagen;
+
+        if(File::exists($imagen_path)){
+            unlink($imagen_path);
+        }
+
         return redirect()->route('posts.index',auth()->user()->username);
     }
 }

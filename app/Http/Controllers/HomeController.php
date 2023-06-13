0e9755e6d70815b47357e8ad946e77469d125d00
nancy <smithnancy@outlook.com>
2023-06-13 22:10:15 +1000

    homeControler now returns the posts of the users the authenticated user follows

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +10,17 @@ class HomeController extends Controller
     public function __invoke()
     {
         //dd('home');
-        return view('home');
+
+        //obtener a quienes seguimos
+        // la parte following->toArray() me trae un array de la respuesta
+        //con pluck() puedo pasale solo los parametros que quiero que returne
+        // dd(auth()->user()->following->pluck('id')->toArray());
+
+        $ids = (auth()->user()->following->pluck('id')->toArray());
+        $posts = Post::whereIn('user_id',$ids)->paginate(20);
+        //dd($posts);
+        return view('home',[
+            'posts' => $posts
+        ]);
     }
 }

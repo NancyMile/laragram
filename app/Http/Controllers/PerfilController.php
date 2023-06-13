77ee83d1a807f9c1b182f7961f8d12d57c04a3f8
nancy <smithnancy@outlook.com>
2023-06-13 15:49:04 +1000

    PerfilController store method validates if ther is any image on db  if not imagen is upoaded,now saving images on profile folder

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -30,6 +32,30 @@ class PerfilController extends Controller
             'username' => ['required','unique:users,username,'.auth()->user()->id,'min:3','max:20'],
         ]);
 
+        if($request->imagen){
+            //dd('si Hay Imagen',$request->file('imagen'));
+            $imagen = $request->file('imagen');
+            // the following may return  something  like {"imagen":"jpg"}
+            //return response()->json(['imagen' => $imagen->extension()]);
 
+            // con el uuid  vamos a  agregarle un aid a la  imagen  para hacerla unica
+            $nombreImagen = Str::uuid().".".$imagen->extension();
+            //crear la imagen el el servidor  import Image de los Facades
+            $imagenServidor = Image::make($imagen);
+            //vamos hacer  que cada imagen fit  1000X1000 pixels
+            $imagenServidor->fit(1000,1000);
+
+            $imagenPath = public_path('perfiles').'/'.$nombreImagen;
+            //mueva la  ruta al servidor  una ve  precesada
+            $imagenServidor->save($imagenPath);
+        }
+
+        //guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? auth()->user()->imagen ?? ''; //nombre de imgen o hay imagen en la db or vacio
+        $usuario->save();
+
+        return  redirect()->route('posts.index',$usuario->username);
     }
 }

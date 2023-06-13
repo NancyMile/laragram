62bcdf383258920525bfb079749e6223495865d7
nancy <smithnancy@outlook.com>
2023-06-13 20:03:04 +1000

    followerController method store implemented now saves the new follower of the visited profile

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FollowerController extends Controller
 {
-    //
+    public function store(User $user){
+        // el usuario al que se va a seguir
+        //dd($user->username);
+
+         /** Dado a que en este cso  amnos usuarios  pertenecen a la misma tabla no  usaremos create
+          * tenemos  que usar attach
+          */
+        // esta va a agregarle al usuario que se va a seguir quien lo esta siguiendo
+        // osea la persona autenticada
+          $user->followers()->attach(auth()->user()->id);
+          return back();
+    }
 }

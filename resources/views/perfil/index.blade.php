c9d724f2e47dec5fad04d090b692959f2e8250e0
nancy <smithnancy@outlook.com>
2023-06-13 13:54:27 +1000

    perfil  indev view a form created toedit user and add image

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -5,5 +5,31 @@
 @endsection
 
 @section('contenido')
-
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-white shadow p-6">
+            <form action="" class="mt-10 md:mt-0">
+                <div class="mb-5">
+                    <label for="username" class="mb-2 text-gray-500 uppercase block font-bold">
+                        Username
+                    </label>
+                    <input type="text" id="username" name="username" placeholder="Tu username"
+                        class=" border p-3  w-full rounded-lg @error('username') border-red-500 @enderror"
+                        value="{{ auth()->user()->username }}" />
+                    @error('username')
+                    <p class="bg-red-500 text-white rounded-lg my-2 text-sm p-2 text-center">
+                        {{ $message }}
+                    </p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="imagen" class="mb-2 text-gray-500 uppercase block font-bold">
+                        Imagen
+                    </label>
+                    <input type="file" id="imagen" name="imagen"
+                        class=" border p-3  w-full rounded-lg" value="" accept=".jpg,.jped,.npg"/>
+                </div>
+                <input type="submit" value="Guardar cambios"  class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold p-3 text-white rounded-lg w-full"/>
+            </form>
+        </div>
+    </div>
 @endsection
